c75ec0c432f3b66ded2035c7c20cff5d92b8c28c
mostafizerrahman <mostafizerrahman.0007@gmail.com>
2026-07-17 05:44:10 -0700

    second commit

---
+++ components/first.jsx
@@ -0,0 +1,10 @@
+const FirstProject = ()=>{
+    return (
+        <>
+            <h1>First Project</h1>
+            <p>This is the first project component.</p>
+        </>
+    )
+}
+
+export default FirstProject;
---
+++ components/second.jsx
@@ -0,0 +1,10 @@
+const SecondProject = ()=>{
+    return (
+        <div>
+            <h1>Second Project</h1>
+            <p>This is the second project component.</p>
+        </div>
+    )
+}
+
+export default SecondProject;
